64536b768f5863a6bc72c4c16fa9c3eb79b7f53b
baoqdau <baoqdau@MacBook-Pro-cua-baoqdau.local>
2024-10-15 19:38:08 +0700

    first

---
+++ test.py
@@ -0,0 +1,24 @@
+import json
+import os
+import uuid
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.service_account import Credentials
+from google.auth import jwt, crypt
+
+
+class DemoLoyalty:
+    """Demo class for creating and managing Loyalty cards in Google Wallet.
+
+    Attributes:
+        key_file_path: Path to service account key file from Google Cloud
+            Console. Environment variable: GOOGLE_APPLICATION_CREDENTIALS.
+        base_url: Base URL for Google Wallet API requests.
+    """
+
+    def __init__(self):
+        self.key_file_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS',
+                                            '/path/to/key.json')
+        # Set up authenticated client
+        self.auth()
